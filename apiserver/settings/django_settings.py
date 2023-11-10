29b271c5bd25ec8ff1ad88918d0923679d208e17
Amal Salvin Joseph <amalsalvin97@gmail.com>
2023-11-10 22:43:20 +0530

    feat: add cors-headers package

--- apiserver/settings/django_settings.py
+++ apiserver/settings/django_settings.py
@@ -41,9 +41,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3P - Production, Preparation, Process
+    "corsheaders",
+    # APPS
+    "accounts",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -125,3 +130,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Cors Headers
+CORS_ALLOW_ALL_ORIGINS = True
